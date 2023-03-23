303ff6b3e5f4a0b4346c66fabf8586fbce2b8424
Taras.Kravets <taraskravets95@gmail.com>
2023-03-24 00:18:37 +0200

    feat(UI): added snippets-section

---
+++ components/snippets/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Snippets() {
+	return (
+		<section className='flex justify-center'>
+			<div className='section-container my-20 px-20'>
+				<h3>Keep track of your snippets</h3>
+				<p className='max-w-3xl mx-auto mb-24 text-xl leading-9 text-center text-grayishBlue'>
+					Clipboard instantly stores any item you copy in the cloud, meaning you
+					can access your snippets immediately on all your devices. Our Mac and
+					iOS apps will help you organize everything.
+				</p>
+			</div>
+		</section>
+	);
+}
