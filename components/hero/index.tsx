f62d5f4b35f62ff3c95297fe48a1d000859c38c8
Taras.Kravets <taraskravets95@gmail.com>
2023-03-23 23:36:05 +0200

    feat(UI): implemented hero-section

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 export default function Hero() {
 	return (
 		<section className='flex justify-center'>
@@ -9,6 +11,27 @@ export default function Hero() {
 					alt='logo image'
 					className='mx-auto my-16'
 				/>
+				<h3 className='mb-8 text-4xl font-bold text-darkGrayishBlue md:text-5xl'>
+					A history of everything you copy
+				</h3>
+				<p className='max-w-3xl mx-auto mb-10 text-2xl text-grayishBlue'>
+					Clipboard allows you to track and organize everything you copy.
+					Instantly access your clipboard on all your devices.
+				</p>
+				<div className='flex flex-col justify-center w-full space-y-6 text-xl text-white md:flex-row md:space-y-0 md:space-x-4'>
+					<Link
+						href='#'
+						className='p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80'
+					>
+						Download for iOS
+					</Link>
+					<Link
+						href='#'
+						className='p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80'
+					>
+						Download for Mac
+					</Link>
+				</div>
 			</div>
 		</section>
 	);
