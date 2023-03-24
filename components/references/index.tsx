19bda8356ab2a80e904d8b8a29b30f6840531863
Taras.Kravets <taraskravets95@gmail.com>
2023-03-24 09:41:20 +0200

    fix(icons): fixed icons supercharge section

---
+++ components/references/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function References() {
+	return (
+		<section>
+			<div className='flex flex-col items-center justify-between max-w-6xl px-10 mx-auto space-y-16 my-44 md:flex-row md:space-y-0'>
+				<img src='assets/logo-google.png' alt='' />
+				<img src='assets/logo-ibm.png' alt='' />
+				<img src='assets/logo-microsoft.png' alt='' />
+				<img src='assets/logo-hp.png' alt='' />
+				<img src='assets/logo-vector-graphics.png' alt='' />
+			</div>
+		</section>
+	);
+}
